a4a3dfc49a517babcc71d36b2091d87020210437
nicolas.garridorincon <ngarridor@udistrital.edu.co>
2025-10-10 23:58:02 -0500

    2do commit, ejercicio de zodiaco por mes y año de nacimiento más validador de mayoría de edad, hay un error en el while que aspiro solucionar al final de expresar todas las funciones en el switch.

--- src/Main.java
+++ src/Main.java
@@ -24,10 +24,15 @@ public class Main {
             opcion = leer.nextInt();
             switch (opcion) {
                 case 1:
+                    signozodiacal();
+                    break;
 
                 case 2:
+                    mayordeedad();
+                    break;
 
                 case 3:
+
 
                 case 4:
 
@@ -45,5 +50,171 @@ public class Main {
             while (opcion > 0) ;
         }
     }
+
+    public static void mayordeedad() {
+        System.out.println(name + ", enter your birth year.");
+        int year = leer.nextInt();
+        year = 2025 - year;
+        if (year >= 18) {
+            System.out.println(name + ", eres mayor de edad.");
+        } else {
+            System.out.println(name + ", no eres mayor de edad.");
+        }
+    }
+
+        public static void signozodiacal () {
+            System.out.println(name + ", ingresa tu dia de nacimiento");
+            int dia = leer.nextInt();
+            System.out.println(name + ", ingresa tu mes de nacimiento en numeros");
+            int mes = leer.nextInt();
+            String signozodiacal = signozodiacal2(dia, mes);
+            System.out.println(name + ", tu signo zodiacal es: " + signozodiacal);
+        }
+
+        public static String signozodiacal2( int dia, int mes){
+            String resultado = "fecha no valida";
+            if (mes >= 1) {
+                if (mes <= 12) {
+
+                    if (dia >= 1) {
+                        if (dia <= 31) {
+
+                            if (mes == 1) {
+                                if (dia <= 19) {
+                                    resultado = "Capricornio";
+                                }
+
+                                if (dia >= 20) {
+                                    resultado = "Acuario";
+                                }
+                            }
+
+                            if (mes == 2) {
+                                if (dia <= 18) {
+                                    resultado = "Acuario";
+                                }
+
+                                if (dia >= 19) {
+                                    resultado = "Piscis";
+                                }
+                            }
+
+                            if (mes == 3) {
+                                if (dia <= 20) {
+                                    resultado = "Piscis";
+                                }
+
+                                if (dia >= 21) {
+                                    resultado = "Aries";
+                                }
+                            }
+
+                            if (mes == 4) {
+                                if (dia <= 19) {
+                                    resultado = "Aries";
+                                }
+
+                                if (dia >= 20) {
+                                    resultado = "Tauro";
+                                }
+                            }
+
+                            if (mes == 5) {
+                                if (dia <= 20) {
+                                    resultado = "Tauro";
+                                }
+
+                                if (dia >= 21) {
+                                    resultado = "Geminis";
+                                }
+                            }
+
+                            if (mes == 6) {
+                                if (dia <= 20) {
+                                    resultado = "Geminis";
+                                }
+
+                                if (dia >= 21) {
+                                    resultado = "Cancer";
+                                }
+                            }
+
+                            if (mes == 7) {
+                                if (dia <= 22) {
+                                    resultado = "Cancer";
+                                }
+
+                                if (dia >= 23) {
+                                    resultado = "Leo";
+                                }
+                            }
+
+                            if (mes == 8) {
+                                if (dia <= 22) {
+                                    resultado = "Leo";
+                                }
+
+                                if (dia >= 23) {
+                                    resultado = "Virgo";
+                                }
+                            }
+
+                            if (mes == 9) {
+                                if (dia <= 22) {
+                                    resultado = "Virgo";
+                                }
+
+                                if (dia >= 23) {
+                                    resultado = "Libra";
+                                }
+                            }
+
+                            if (mes == 10) {
+                                if (dia <= 22) {
+                                    resultado = "Libra";
+                                }
+
+                                if (dia >= 23) {
+                                    resultado = "Escorpio";
+                                }
+                            }
+
+                            if (mes == 11) {
+                                if (dia <= 21) {
+                                    resultado = "Escorpio";
+                                }
+
+                                if (dia >= 22) {
+                                    resultado = "Sagitario";
+                                }
+                            }
+
+                            if (mes == 12) {
+                                if (dia <= 21) {
+                                    resultado = "Sagitario";
+                                }
+
+                                if (dia >= 22) {
+                                    resultado = "Capricornio";
+                                }
+                            }
+
+                        } else {
+                            resultado = "error, el dia que ingresaste es mayor a 31, intenta de nuevo";
+                        }
+                    } else {
+                        resultado = "error, el dia que ingresaste es menor a 1, intenta de nuevo";
+                    }
+                } else {
+                    resultado = "error, el mes que ingresaste es mayor que 12, intenta de nuevo";
+                }
+            } else {
+                resultado = "error, el mes que ingresaste es menor que 1, intenta de nuevo";
+            }
+
+            return resultado;
+        }
+    }
+}
 
 
